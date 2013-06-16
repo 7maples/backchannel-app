379e5e7cce5c2efa662db87c4f7f5d38daaa7f2a
Jennifer Eliuk <jen7maples@gmail.com>
2013-06-16 15:14:37 -0600

    Mock up landing page layout with chat window, basic schedule, twitter log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # skip_before_filter :verify_authenticity_token, :only => :create
-
 
   private
 
--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -2,6 +2,8 @@ class ConferencesController < ApplicationController
   def show
     @conference = ConferenceFetcher.get(params[:conference])
 
+    #@conference = OpenStruct.new({hash})
+
     render :status => 404 if @conference.nil?
   end
 end
