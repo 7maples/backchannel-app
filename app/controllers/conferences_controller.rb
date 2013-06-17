66d8a47adc7df854f9d9a0f7be9b3843008718c1
Jennifer Eliuk <jen7maples@gmail.com>
2013-06-16 19:27:32 -0600

    Start playing with hardcoded conf data to determine how to parse. Need to know actual data structure.

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,8 +1,20 @@
 class ConferencesController < ApplicationController
   def show
-    @conference = ConferenceFetcher.get(params[:conference])
+    # @conference = ConferenceFetcher.get(params[:conference])
 
-    #@conference = OpenStruct.new({hash})
+    @conference = OpenStruct.new({ name: "RubyConf 2013",
+                                   track_details: { track_id: 1,
+                                   title: "Refactoring",
+                                   description: "Talks about how to clean up your code",
+                                   location: "Banquet Hall",
+                                   session_details: { session_id: 1,
+                                   title: "Refactoring Classes",
+                                   description: "Make your classes leaner",
+                                   topic_one: "Ruby",
+                                   topic_two: "refactoring",
+                                   topic_three: "DRY",
+                                   speaker: "Katrina Owen",
+                                   location: "Room 413" }}})
 
     render :status => 404 if @conference.nil?
   end
