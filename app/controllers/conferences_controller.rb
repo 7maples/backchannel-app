07cb5713f7724617b5eac3b8ac9a0bb5765cf607
Kareem Grant <kareem@getuserwise.com>
2013-06-20 03:32:52 -0600

    Updated conferences#show page to better display schedule being pulled from the Scheduler service

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,7 +1,6 @@
 class ConferencesController < ApplicationController
   def show
     @conference = ConferenceFetcher.get(params[:conference])
-    # raise @conference.inspect
     render :status => 404 if @conference.nil?
   end
 end
