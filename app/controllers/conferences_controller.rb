a91f30ef6465c3710e476a0e1d6577df5fc79638
Kareem Grant <kareem@getuserwise.com>
2013-06-20 11:03:31 -0600

    Fixed Twitter auth issue (added credentials), also added redirect logic to ensure user is redirected to page from which they logged in

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,6 +1,15 @@
 class ConferencesController < ApplicationController
+
+  before_filter :set_session, only: :show
+
   def show
     @conference = ConferenceFetcher.get(params[:conference])
     render :status => 404 if @conference.nil?
+  end
+
+  private
+
+  def set_session
+    session[:next_page] = conference_path(params[:conference])
   end
 end
