4d99c5ece1476adf0a62692f37cf4f1a9eff277f
Raphael Weiner <raphael.weiner@gmail.com>
2013-07-03 12:49:43 -0500

    add full host to omniauth config'

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 OmniAuth.config.logger = Rails.logger
+OmniAuth.config.full_host = "http://backchannel.pw"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
